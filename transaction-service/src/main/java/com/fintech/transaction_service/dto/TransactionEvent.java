47fb3b0cfe9a7b3e82e93f5a2a793be32600d2fa
user.name "Amaan <user.email "amaanhussain1706@gmail.com>
2026-06-25 19:19:17 +0530

    implement kafka producer for transaction events

---
+++ transaction-service/src/main/java/com/fintech/transaction_service/dto/TransactionEvent.java
@@ -0,0 +1,12 @@
+package com.fintech.transaction_service.dto;
+
+import java.math.BigDecimal;
+
+public record TransactionEvent(
+        String referenceId,
+        Long accountId,
+        BigDecimal amount,
+        String type,
+        String status,
+        String message
+) {}
